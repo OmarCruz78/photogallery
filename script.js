1f3e0ba4cf3696122a6f75cddd1355d7bb18a60a
OmarCruz78 <harekrshnayoga@gmail.com>
2025-02-16 16:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,37 @@
 /* body onload*/
+/* body onload*/
 function tabindex(){
-currentImages = document.querySelectorAll(".carrousel img")
-  for ( var i=0; i < currentImages.length; i++){
-    currentImages[i].setAttribute("tabindex","0");
-  }
+    const currentImages = document.querySelectorAll(".carrousel img");
+    for (let i = 0; i < currentImages.length; i++){
+        currentImages[i].setAttribute("tabindex","0");
+    }
 }
 
 function upDate(previewPic){
-imagen = document.getElementById('image');
-main = document.getElementById('main');
-imagen.style.backgroundImage = "url('" + previewPic.src + "')";               
-imagen.innerHTML=previewPic.alt;
+    const imagen = document.getElementById('image');
+    imagen.style.backgroundImage = "url('" + previewPic.src + "')";
+    imagen.innerHTML = previewPic.alt;
 }
 
-function upDate2(previewPic){
-  imagen = document.getElementById('image');
-  imagen.style.display = "block";
-  imagen.style.z-index = "999";
-imagen.style.position = "absolute";               
-imagen.style.top = "0";
-imagen.style.left = "0";
-  imagen.style.right = "0";
-   imagen.style.leftt = "0";
-  imagen.style.margin = "auto";
-main.style.margin = "auto";  
-  top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-margin: auto;
+function upDate2(previewPic){ // Corrected and improved
+    const imagen = document.getElementById('image');
+    imagen.style.backgroundImage = "url('" + previewPic.src + "')";
+    imagen.style.display = "block";
+    imagen.style.zIndex = "999";
+    imagen.style.position = "absolute";
+    imagen.style.top = "0";
+    imagen.style.left = "0";
+    imagen.style.right = "0";
+    imagen.style.bottom = "0";
+    imagen.style.margin = "auto";
 }
-
 
 function unblur(){
-           /* currentImages = document.querySelectorAll(".carrousel img")
-  for ( var i = 0; i < currentImages.length; i++){
-    currentImages[i].style.height = "50px";*/
-	}
+    // Add code here to unblur images if needed
+}
 
 function unDo(){
-imagen = document.getElementById('image');
-imagen.innerHTML='Hover over an image below to display here.';
-imagen.style.backgroundImage = "url('https://i.ibb.co/RKYDgT2/collage-naupa-iglesia.jpg')";
-	}
-
-		
-function showImagen(){
-  imagen = document.getElementById('image');
-    imagen.style.display = "block";
-  imagen.style.z-index = "999";
-imagen.style.position = "absolute";               
-imagen.style.top = "0";
-imagen.style.left = "0";
-  imagen.style.right = "0";
-   imagen.style.leftt = "0";
-  imagen.style.margin = "auto";
-main.style.margin = "auto";  
-  
-}	
+    const imagen = document.getElementById('image');
+    imagen.innerHTML = 'Hover over an image below to display here.';
+    imagen.style.backgroundImage = "url('https://i.ibb.co/RKYDgT2/collage-naupa-iglesia.jpg')";
+}
